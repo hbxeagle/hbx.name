3f2681f25d47939b0c89c37613bf031f9ae0c71f
hbxeagle <hbxeagle@gmail.com>
2015-01-25 18:03:13 +0800

    modify web.js

--- web.js
+++ web.js
@@ -3,3 +3,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/dist'));
+app.get('/', function(request, response) {
+  var result = '';
+  var times = 5;
+  for (i=0; i< times; i++) {
+    result += '--|';
+  }
+  response.send(result);
+});
